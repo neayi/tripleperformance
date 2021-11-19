6a3be8badd35f93c51858a730c9ba4ee6f1a1cbe
Bertrand Gorge <bertrand.gorge@neayi.com>
2021-11-19 16:54:20 +0100

    Removed discourse

--- discourse/backup.sh
+++
@@ -1,2 +0,0 @@
-
-docker-compose run --rm -v /var/www/tripleperformance_docker/discourse:/pgpass:ro -e DISABLE_WELCOME_MESSAGE=yes -e PGPASSFILE=/pgpass/.pgpass postgresql pg_dump -F c -U postgres -h postgresql bitnami_discourse > discourse.sql.dump
