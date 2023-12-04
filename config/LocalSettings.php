5a031162cc489c6bc59fe993927b840ab8c08c35
Bertrand Gorge <bertrand.gorge@neayi.com>
2023-12-04 19:16:34 +0100

    Added Matomo

--- config/LocalSettings.php
+++ config/LocalSettings.php
@@ -391,6 +391,20 @@ if('prod' === $env) {
     $wgGroupPermissions['bureaucrat']['gtag-exempt'] = true;
 
     $wgHeadScriptCode .= <<<'START_END_MARKER'
+    <!-- Matomo -->
+    <script>
+    var _paq = window._paq = window._paq || [];
+    /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+    _paq.push(['trackPageView']);
+    _paq.push(['enableLinkTracking']);
+    (function() {
+        var u="//matomo.tripleperformance.fr/";
+        _paq.push(['setTrackerUrl', u+'matomo.php']);
+        _paq.push(['setSiteId', '1']);
+        var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+        g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+    })();
+    </script>
     <!-- Facebook Pixel Code -->
     <script>
       !function(f,b,e,v,n,t,s)
