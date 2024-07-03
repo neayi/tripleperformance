5e37571827c6a8abb1b67a1c4e7b88cc0113b463
Bertrand Gorge <bertrand.gorge@neayi.com>
2024-07-03 17:57:22 +0200

    Added insights_cron.sh

---
+++ insights_cron.sh
@@ -0,0 +1,2 @@
+docker compose -p tripleperformance_prod -f /var/www/tripleperformance_docker/docker-compose.prod.yml exec -T --user="www-data:www-data" insights_preprod bash -c "/usr/local/bin/php /var/www/html/artisan schedule:run"
+docker compose -p tripleperformance_prod -f /var/www/tripleperformance_docker/docker-compose.prod.yml exec -T --user="www-data:www-data" insights bash -c "/usr/local/bin/php /var/www/html/artisan schedule:run"
