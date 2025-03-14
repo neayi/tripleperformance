b8e0994d435daad6042a77ae3fa09ce6466beab0
Bertrand Gorge <bertrand.gorge@neayi.com>
2025-03-14 17:10:45 +0100

    Fixed pageforms that out of the blue is not compatible with REL1_39

--- bin/build_project.php
+++ bin/build_project.php
@@ -179,7 +179,7 @@ function getWiki()
 	$wiki_install_dir = '/html';
 	$wiki_version = 'REL1_39';
 
-	return array(	'git' => '--branch '.$wiki_version.' https://github.com/wikimedia/mediawiki.git',
+	return array(	'git' => 'https://github.com/wikimedia/mediawiki.git',
 					'dest' => $wiki_install_dir,
 					'branch' => $wiki_version);
 }
@@ -212,64 +212,64 @@ function getWikiComponents()
 
 	// https://www.mediawiki.org/wiki/Extension:HeadScript
 	$components[] = array(	'dest' => $wiki_extensions_dir . '/HeadScript',
-							'git' => '--branch '.$wiki_version.' https://github.com/wikimedia/mediawiki-extensions-HeadScript.git',
+							'git' => 'https://github.com/wikimedia/mediawiki-extensions-HeadScript.git',
 							'branch' => $wiki_version);
 
 	$components[] = array(	'dest' => $wiki_extensions_dir . '/EmbedVideo',
 							'git' => 'https://github.com/StarCitizenWiki/mediawiki-extensions-EmbedVideo.git');
 
 	$components[] = array(	'dest' => $wiki_extensions_dir . '/RelatedArticles',
-							'git' => '--branch '.$wiki_version.' https://github.com/wikimedia/mediawiki-extensions-RelatedArticles.git',
+							'git' => 'https://github.com/wikimedia/mediawiki-extensions-RelatedArticles.git',
 							'branch' => $wiki_version);
 
 	$components[] = array(	'dest' => $wiki_extensions_dir . '/Popups',
-							'git' => '--branch '.$wiki_version.' https://github.com/wikimedia/mediawiki-extensions-Popups.git',
+							'git' => 'https://github.com/wikimedia/mediawiki-extensions-Popups.git',
 							'branch' => $wiki_version);
 
 	$components[] = array(	'dest' => $wiki_extensions_dir . '/Description2',
-							'git' => '--branch '.$wiki_version.' https://github.com/wikimedia/mediawiki-extensions-Description2.git',
+							'git' => 'https://github.com/wikimedia/mediawiki-extensions-Description2.git',
 							'branch' => $wiki_version);
 
 	$components[] = array(	'dest' => $wiki_extensions_dir . '/Loops',
-							'git' => '--branch '.$wiki_version.' https://github.com/wikimedia/mediawiki-extensions-Loops.git',
+							'git' => 'https://github.com/wikimedia/mediawiki-extensions-Loops.git',
 							'branch' => $wiki_version);
 
 	$components[] = array(	'dest' => $wiki_extensions_dir . '/Variables',
-							'git' => '--branch '.$wiki_version.' https://github.com/wikimedia/mediawiki-extensions-Variables.git',
+							'git' => 'https://github.com/wikimedia/mediawiki-extensions-Variables.git',
 							'branch' => $wiki_version);
 
 	$components[] = array(	'dest' => $wiki_extensions_dir . '/PluggableAuth',
-							'git' => '--branch '.$wiki_version.' https://github.com/wikimedia/mediawiki-extensions-PluggableAuth.git',
+							'git' => 'https://github.com/wikimedia/mediawiki-extensions-PluggableAuth.git',
 							'branch' => $wiki_version);
 
 	$components[] = array(	'dest' => $wiki_extensions_dir . '/Echo',
-							'git' => '--branch '.$wiki_version.' https://github.com/wikimedia/mediawiki-extensions-Echo.git',
+							'git' => 'https://github.com/wikimedia/mediawiki-extensions-Echo.git',
 							'branch' => $wiki_version);
 
 	$components[] = array(	'dest' => $wiki_extensions_dir . '/DeleteBatch',
-							'git' => '--branch '.$wiki_version.' https://github.com/wikimedia/mediawiki-extensions-DeleteBatch.git',
+							'git' => 'https://github.com/wikimedia/mediawiki-extensions-DeleteBatch.git',
 							'postinstall' => 'composer',
 							'branch' => $wiki_version);
 
 	$components[] = array(	'dest' => $wiki_extensions_dir . '/Disambiguator',
-							'git' => '--branch '.$wiki_version.' https://github.com/wikimedia/mediawiki-extensions-Disambiguator.git',
+							'git' => 'https://github.com/wikimedia/mediawiki-extensions-Disambiguator.git',
 							'branch' => $wiki_version);
 
 	$components[] = array(	'dest' => $wiki_extensions_dir . '/VisualEditor',
-							'git' => '--branch '.$wiki_version.' https://github.com/wikimedia/mediawiki-extensions-VisualEditor.git',
+							'git' => 'https://github.com/wikimedia/mediawiki-extensions-VisualEditor.git',
 							'postinstall' => 'submodules',
 							'branch' => $wiki_version);
 
 	$components[] = array(	'dest' => $wiki_extensions_dir . '/VEForAll',
-							'git' => '--branch '.$wiki_version.' https://github.com/wikimedia/mediawiki-extensions-VEForAll.git',
+							'git' => 'https://github.com/wikimedia/mediawiki-extensions-VEForAll.git',
 							'branch' => $wiki_version);
 
 	$components[] = array(	'dest' => $wiki_extensions_dir . '/UploadWizard',
-							'git' => '--branch '.$wiki_version.' https://github.com/wikimedia/mediawiki-extensions-UploadWizard.git',
+							'git' => 'https://github.com/wikimedia/mediawiki-extensions-UploadWizard.git',
 							'branch' => $wiki_version);
 
 	$components[] = array(	'dest' => $wiki_extensions_dir . '/HidePrefix',
-							'git' => '--branch '.$wiki_version.' https://github.com/wikimedia/mediawiki-extensions-HidePrefix.git',
+							'git' => 'https://github.com/wikimedia/mediawiki-extensions-HidePrefix.git',
 							'branch' => $wiki_version);
 
 	// WikiSearch
@@ -277,27 +277,29 @@ function getWikiComponents()
 							'git' => 'https://github.com/Open-CSP/WikiSearch.git',
 							'postinstall' => 'composer');
 	$components[] = array(	'dest' => $wiki_extensions_dir . '/WikiSearchFront',
-							'git' => '--branch Neayi.v3 https://github.com/neayi/WikiSearchFront.git');
+							'git' => 'https://github.com/neayi/WikiSearchFront.git',
+							'branch' => 'Neayi.v3');
 
 
 	// Extensions maintained by Yaron Koren which should work better on the master branch:
 	$components[] = array(	'dest' => $wiki_extensions_dir . '/PageForms',
-							'git' => 'https://github.com/wikimedia/mediawiki-extensions-PageForms.git');
+							'git' => 'https://github.com/wikimedia/mediawiki-extensions-PageForms.git',
+							'branch' => '5.9.1'); // 5.9.1 is the last version that works with REL1_39 (go back to master when moving to REL1_42)
 
 	// Other extensions
 	$components[] = array(	'dest' => $wiki_extensions_dir . '/Realnames',
 							'git' => 'https://github.com/ofbeaton/mediawiki-realnames.git');
 
 	$components[] = array(	'dest' => $wiki_extensions_dir . '/AdminLinks',
-							'git' => '--branch '.$wiki_version.' https://github.com/wikimedia/mediawiki-extensions-AdminLinks.git',
+							'git' => 'https://github.com/wikimedia/mediawiki-extensions-AdminLinks.git',
 							'branch' => $wiki_version);
 
 	$components[] = array(	'dest' => $wiki_extensions_dir . '/UrlShortener',
-							'git' => '--branch '.$wiki_version.' https://github.com/wikimedia/mediawiki-extensions-UrlShortener.git',
+							'git' => 'https://github.com/wikimedia/mediawiki-extensions-UrlShortener.git',
 							'branch' => $wiki_version);
 
 	$components[] = array(	'dest' => $wiki_extensions_dir . '/ChangeAuthor',
-							'git' => '--branch '.$wiki_version.' https://github.com/wikimedia/mediawiki-extensions-ChangeAuthor.git',
+							'git' => 'https://github.com/wikimedia/mediawiki-extensions-ChangeAuthor.git',
 							'branch' => $wiki_version);
 
 	$components[] = array(	'dest' => $wiki_extensions_dir . '/RottenLinks',
@@ -307,29 +309,29 @@ function getWikiComponents()
 							'git' => 'https://github.com/kulttuuri/SlackNotifications.git');
 
 	$components[] = array(	'dest' => $wiki_extensions_dir . '/OpenGraphMeta',
-							'git' => '--branch '.$wiki_version.' https://github.com/wikimedia/mediawiki-extensions-OpenGraphMeta.git',
+							'git' => 'https://github.com/wikimedia/mediawiki-extensions-OpenGraphMeta.git',
 							'branch' => $wiki_version);
 
 	$components[] = array(	'dest' => $wiki_extensions_dir . '/LinkTitles',
-							'git' => '--branch fix-68 https://github.com/neayi/LinkTitles.git',
+							'git' => 'https://github.com/neayi/LinkTitles.git',
 							'branch' => 'fix-68');
 
 	$components[] = array(	'dest' => $wiki_extensions_dir . '/Math',
-							'git' => '--branch '.$wiki_version.' https://gerrit.wikimedia.org/r/mediawiki/extensions/Math',
+							'git' => 'https://gerrit.wikimedia.org/r/mediawiki/extensions/Math',
 							'branch' => $wiki_version);
 
 	// Translation
 	// $components[] = array(	'dest' => $wiki_extensions_dir . '/UniversalLanguageSelector',
-	// 						'git' => '--branch '.$wiki_version.' https://gerrit.wikimedia.org/r/mediawiki/extensions/UniversalLanguageSelector.git',
+	// 						'git' => 'https://gerrit.wikimedia.org/r/mediawiki/extensions/UniversalLanguageSelector.git',
 	// 						'branch' => $wiki_version,
 	// 						'postinstall' => 'composer');
 	// $components[] = array(	'dest' => $wiki_extensions_dir . '/Translate',
-	// 						'git' => '--branch '.$wiki_version.' https://github.com/wikimedia/mediawiki-extensions-Translate.git',
+	// 						'git' => 'https://github.com/wikimedia/mediawiki-extensions-Translate.git',
 	// 						'branch' => $wiki_version,
 	// 						'postinstall' => 'composer');
 
 	$components[] = array(	'dest' => $wiki_extensions_dir . '/NativeSvgHandler',
-							'git' => '--branch '.$wiki_version.' https://github.com/wikimedia/mediawiki-extensions-NativeSvgHandler.git',
+							'git' => 'https://github.com/wikimedia/mediawiki-extensions-NativeSvgHandler.git',
 							'branch' => $wiki_version);
 
 	// Neayi extensions and forks
@@ -337,12 +339,12 @@ function getWikiComponents()
 	                        'git' => 'https://github.com/neayi/PDFEmbed.git'); // Forked from https://github.com/miraheze/PDFEmbed.git to remove the check on users
 
 	$components[] = array(	'dest' => $wiki_extensions_dir . '/HitCounters',
-							'git' => '--branch Neayi1_39 https://github.com/neayi/mediawiki-extensions-HitCounters.git',
+							'git' => 'https://github.com/neayi/mediawiki-extensions-HitCounters.git',
 							'branch' => 'Neayi1_39',
 							'postinstall' => 'composer');
 
 	$components[] = array(	'dest' => $wiki_extensions_dir . '/Link_Attributes',
-							'git' => '--branch Neayi https://github.com/neayi/mediawiki-extensions-Link_Attributes.git',
+							'git' => 'https://github.com/neayi/mediawiki-extensions-Link_Attributes.git',
 							'branch' => 'Neayi');
 
 	$components[] = array(	'dest' => $wiki_skins_dir . '/skin-neayi',
@@ -352,7 +354,8 @@ function getWikiComponents()
 							'git' => 'https://github.com/neayi/ext-carousel.git');
 
 	$components[] = array(	'dest' => $wiki_extensions_dir . '/NeayiAuth',
-							'git' => '--branch '.$wiki_version.' https://github.com/neayi/NeayiAuth.git',
+							'git' => 'https://github.com/neayi/NeayiAuth.git',
+							'branch' => $wiki_version,
 							'postinstall' => 'composer');
 
 	$components[] = array(	'dest' => $wiki_extensions_dir . '/ConvertPDF2Wiki',
@@ -360,17 +363,17 @@ function getWikiComponents()
 							
 	// TODO: Create a new repo and get rid of CommentStreams
 	$components[] = array(	'dest' => $wiki_extensions_dir . '/DiscourseIntegration',
-							'git' => '--branch main https://github.com/neayi/mw-DiscourseIntegration.git',
+							'git' => 'https://github.com/neayi/mw-DiscourseIntegration.git',
 							'postinstall' => 'submodules',
 							'branch' => 'main');
 
 	$components[] = array(	'dest' => $wiki_extensions_dir . '/InputBox',
-							'git' => '--branch '.$neayi_wiki_version.' https://github.com/neayi/mediawiki-extensions-InputBox.git',
+							'git' => 'https://github.com/neayi/mediawiki-extensions-InputBox.git',
 							'branch' => $neayi_wiki_version);
 
 	// TODO: Merge integration-of-discourse in master
 	$components[] = array(	'dest' => $wiki_extensions_dir . '/NeayiInteractions',
-							'git' => '--branch integration-of-discourse https://github.com/neayi/mw-NeayiInteractions.git',
+							'git' => 'https://github.com/neayi/mw-NeayiInteractions.git',
 							'branch' => 'integration-of-discourse');
 
 	$components[] = array(	'dest' => $wiki_extensions_dir . '/NeayiNavbar',
@@ -458,7 +461,11 @@ function checkout_project($aComponent, $bForceRemoveFolder = false)
 			throw new \RuntimeException('The dest dir already exists: '. root_web . $aComponent['dest']);
     }
 
-	$cmd = 'git clone --depth 1 -q ' . $aComponent['git'] . ' ' . root_web . $aComponent['dest'];
+	if (!empty($aComponent['branch']))
+		$cmd = 'git clone --depth 1 -q --branch ' . $aComponent['branch'] . ' ' . $aComponent['git'] . ' ' . root_web . $aComponent['dest'];
+	else
+		$cmd = 'git clone --depth 1 -q ' . $aComponent['git'] . ' ' . root_web . $aComponent['dest'];
+
 	runCommand($cmd);
 
 	if (!empty($aComponent['tag']))
