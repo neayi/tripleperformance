191a0a14b66e82ec4e2a75a9935088c2ba3a38bf
Bertrand Gorge <bertrand.gorge@neayi.com>
2025-10-10 18:24:14 +0200

    Fixed the version of semantic scribunto

--- bin/build_project.php
+++ bin/build_project.php
@@ -199,7 +199,7 @@ function getWikiComponents()
 	$components[] = array(	'composer' => 'mediawiki/semantic-media-wiki "^6.0.1"' ); // https://packagist.org/packages/mediawiki/semantic-media-wiki
 	$components[] = array(	'composer' => 'mediawiki/maps "^12.0"' ); // https://packagist.org/packages/mediawiki/maps
 	$components[] = array(	'composer' => 'mediawiki/semantic-result-formats "^5.1.0"' ); // https://packagist.org/packages/mediawiki/semantic-result-formats
-	$components[] = array(	'composer' => 'mediawiki/semantic-scribunto "2.4.0.x-dev"' ); // https://packagist.org/packages/mediawiki/semantic-scribunto
+	$components[] = array(	'composer' => 'mediawiki/semantic-scribunto "^2.3.3"' ); // https://packagist.org/packages/mediawiki/semantic-scribunto
 	$components[] = array(	'composer' => 'mediawiki/semantic-extra-special-properties "^4.0"' ); // https://packagist.org/packages/mediawiki/semantic-extra-special-properties
 	$components[] = array(	'composer' => 'mediawiki/iframe-tag "^1.1.1"' ); // https://packagist.org/packages/mediawiki/iframe-tag
 	$components[] = array(	'composer' => 'mediawiki/page-forms "^6.0.1"' ); // https://packagist.org/packages/mediawiki/page-forms
